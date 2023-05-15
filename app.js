5f0bccfcbd72cb36ed414c9c0bf392cc4834b436
Ozan Sevkin <sevkinozan@gmail.com>
2023-05-15 22:20:09 +1000

    feat: add mongoose and connect db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -6,3 +7,5 @@ const { PORT = 3001 } = process.env;
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}...`);
 });
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
