c13821277cab83d3f0801bed2249d30e7a3fa6d0
Ozan Sevkin <sevkinozan@gmail.com>
2023-05-19 20:41:38 +1000

    refactor: add line after last require

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const ERROR_CODE = require("../utils/errors");
 
 router.use("/items", require("./clothingItems"));
 router.use("/users", require("./users"));
+
 router.use((req, res) => {
   res.status(ERROR_CODE.DocumentNotFoundError).send({
     message: "Requested resource not found",
