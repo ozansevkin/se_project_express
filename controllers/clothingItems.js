1de1e4166d82f0f0496c5ee728e98f646afc86d7
Ozan Sevkin <sevkinozan@gmail.com>
2023-05-19 20:36:44 +1000

    refactor: add return statement to the end of catch blocks

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -4,9 +4,7 @@ const ERROR_CODE = require("../utils/errors");
 const getItems = (req, res) => {
   ClothingItem.find({})
     .orFail()
-    .then((items) => {
-      res.send({ data: items });
-    })
+    .then((items) => res.send({ data: items }))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
@@ -23,15 +21,13 @@ const createItem = (req, res) => {
   const ownerId = req.user._id;
 
   ClothingItem.create({ name, weather, imageUrl, owner: ownerId })
-    .then((item) => {
-      res.status(201).send({ data: item });
-    })
+    .then((item) => res.status(201).send({ data: item }))
     .catch((err) => {
       if (err.name === "ValidationError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
       }
 
-      res.status(ERROR_CODE.ServerError).send({
+      return res.status(ERROR_CODE.ServerError).send({
         message: `An error has occurred on the server.`,
       });
     });
@@ -40,15 +36,13 @@ const createItem = (req, res) => {
 const deleteItem = (req, res) => {
   ClothingItem.findByIdAndDelete(req.params.itemId)
     .orFail()
-    .then(() => {
-      res.send({});
-    })
+    .then(() => res.send({}))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError" || err.name === "CastError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
       }
 
-      res.status(ERROR_CODE.ServerError).send({
+      return res.status(ERROR_CODE.ServerError).send({
         message: `An error has occurred on the server.`,
       });
     });
@@ -61,15 +55,13 @@ const likeItem = (req, res) => {
     { new: true }
   )
     .orFail()
-    .then((item) => {
-      res.send({ data: item });
-    })
+    .then((item) => res.send({ data: item }))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError" || err.name === "CastError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
       }
 
-      res.status(ERROR_CODE.ServerError).send({
+      return res.status(ERROR_CODE.ServerError).send({
         message: `An error has occurred on the server.`,
       });
     });
@@ -82,15 +74,13 @@ const unlikeItem = (req, res) => {
     { new: true }
   )
     .orFail()
-    .then((item) => {
-      res.send({ data: item });
-    })
+    .then((item) => res.send({ data: item }))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError" || err.name === "CastError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
       }
 
-      res.status(ERROR_CODE.ServerError).send({
+      return res.status(ERROR_CODE.ServerError).send({
         message: `An error has occurred on the server.`,
       });
     });
--- controllers/users.js
+++ controllers/users.js
@@ -4,15 +4,13 @@ const ERROR_CODE = require("../utils/errors");
 const getUsers = (req, res) => {
   User.find({})
     .orFail()
-    .then((users) => {
-      res.send({ data: users });
-    })
+    .then((users) => res.send({ data: users }))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
       }
 
-      res.status(ERROR_CODE.ServerError).send({
+      return res.status(ERROR_CODE.ServerError).send({
         message: `An error has occurred on the server.`,
       });
     });
@@ -21,15 +19,13 @@ const getUsers = (req, res) => {
 const getUser = (req, res) => {
   User.findById(req.params.userId)
     .orFail()
-    .then((user) => {
-      res.send({ data: user });
-    })
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError" || err.name === "CastError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
       }
 
-      res.status(ERROR_CODE.ServerError).send({
+      return res.status(ERROR_CODE.ServerError).send({
         message: `An error has occurred on the server.`,
       });
     });
@@ -39,15 +35,13 @@ const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
   User.create({ name, avatar })
-    .then((user) => {
-      res.send({ data: user });
-    })
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === "ValidationError") {
         return res.status(ERROR_CODE[err.name]).send({ message: err.message });
       }
 
-      res.status(ERROR_CODE.ServerError).send({
+      return res.status(ERROR_CODE.ServerError).send({
         message: `An error has occurred on the server.`,
       });
     });
