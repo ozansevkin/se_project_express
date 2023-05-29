1f70a037371a212b140d6926c7f7d774660dda74
Ozan Sevkin <sevkinozan@gmail.com>
2023-05-29 20:55:35 +1000

    feat: add email & password fields to the userSchema

--- models/user.js
+++ models/user.js
@@ -7,14 +7,30 @@ const userSchema = new mongoose.Schema({
     required: true,
     minLength: 2,
     maxLength: 30,
+    default: "Elise Bouer",
   },
   avatar: {
     type: String,
     required: true,
+    default:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png",
     validate: {
       validator: (value) => validator.isURL(value),
       message: "You must enter a valid URL",
     },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: "You must enter a valid Email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
   },
 });
 
