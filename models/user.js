3a724a9373ec469782eb27804e19a992c780b698
Ozan Sevkin <sevkinozan@gmail.com>
2023-05-30 23:48:40 +1000

    feat: hide user password

--- models/user.js
+++ models/user.js
@@ -32,28 +32,31 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false, // This way, the user's password hash won't be returned from the database by default
   },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).then((user) => {
-    const error = new Error("Incorrect email or password");
-    error.name = "UnauthorizedError";
+  return this.findOne({ email })
+    .select("+password") // to get the password hash in the user object
+    .then((user) => {
+      const error = new Error("Incorrect email or password");
+      error.name = "UnauthorizedError";
 
-    // not found - reject promise
-    if (!user) {
-      return Promise.reject(error);
-    }
-
-    // found - compare hashes
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+      // not found - reject promise
+      if (!user) {
         return Promise.reject(error);
       }
 
-      return user;
+      // found - compare hashes
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(error);
+        }
+
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model("User", userSchema);
