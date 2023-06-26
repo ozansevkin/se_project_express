93c7c375d315360dda2d9e01eb86be54c5076215
Ozan Sevkin <sevkinozan@gmail.com>
2023-06-26 22:24:16 +1000

    fix: remove unnecassary requirements at user model and validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -27,13 +27,11 @@ const validateItem = celebrate({
 
 const validateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30).messages({
-      "string.empty": 'The "name" field must be filled in',
+    name: Joi.string().min(2).max(30).messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
     }),
-    avatar: Joi.string().required().custom(validateURL).messages({
-      "string.empty": 'The "avatar" field must be filled in',
+    avatar: Joi.string().custom(validateURL).messages({
       "string.uri": 'The "avatar" field must be a valid url',
     }),
     email: Joi.string().required().email().messages({
--- models/user.js
+++ models/user.js
@@ -6,14 +6,12 @@ const UnauthorizedError = require("../errors/Unauthorized");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minLength: 2,
     maxLength: 30,
     default: "Elise Bouer",
   },
   avatar: {
     type: String,
-    required: true,
     default:
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png",
     validate: {
