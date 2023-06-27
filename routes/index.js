a59e0727d1a54f740f1bd24caa97fd2fb8511698
Ozan Sevkin <sevkinozan@gmail.com>
2023-06-27 15:25:34 +1000

    fix: add else condition to if statement

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,11 @@ router.use("/items", require("./clothingItems"));
 router.use("/users", require("./users"));
 
 router.use((err, req, res, next) => {
-  if (err) next(err);
-
-  next(new NotFoundError("Requested resource not found."));
+  if (err) {
+    next(err);
+  } else {
+    next(new NotFoundError("Requested resource not found."));
+  }
 });
 
 module.exports = router;
