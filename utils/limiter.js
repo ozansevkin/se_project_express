5a233bd41996a2d38a1ef432758491c137e79977
Ozan Sevkin <sevkinozan@gmail.com>
2023-07-01 16:01:32 +1000

    feat: increase max request limit in development mode

--- utils/limiter.js
+++ utils/limiter.js
@@ -1,8 +1,9 @@
 const rateLimit = require("express-rate-limit");
+const { NODE_ENV } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: NODE_ENV === "production" ? 100 : 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
