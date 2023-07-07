319f1cce6933e603baa5c6f115c434638cdf5116
Ozan Sevkin <sevkinozan@gmail.com>
2023-07-07 18:41:35 +1000

    fix: update project pepo in config file for pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,8 @@ module.exports = {
     production: {
       user: "sevkinozan",
       host: ["34.168.61.220"],
-      key: "~/.ssh/id_ed25519.pub",
       ref: "origin/main",
-      repo: "git@github.com:ozansevkin/se_project_express.git",
+      repo: "https://github.com/ozansevkin/se_project_express.git",
       path: "/home/sevkinozan",
       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js",
     },
