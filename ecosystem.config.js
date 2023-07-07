5be29e29788485af009d8716f574a7b531566034
Ozan Sevkin <sevkinozan@gmail.com>
2023-07-07 18:51:15 +1000

    fix: update project path in config file for pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       host: ["34.168.61.220"],
       ref: "origin/main",
       repo: "https://github.com/ozansevkin/se_project_express.git",
-      path: "/home/sevkinozan",
+      path: "/home/sevkinozan/se_project_express",
       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js",
     },
   },
